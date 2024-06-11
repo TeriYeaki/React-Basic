d7ace7ceb23fd4743b550126143ce88352a34b90
TeriYeaki <118505370+TeriYeaki@users.noreply.github.com>
2024-06-11 23:21:53 +1000

    add component to main app

--- react-basic-app/src/App.jsx
+++ react-basic-app/src/App.jsx
@@ -1,17 +1,16 @@
 import Header from './components/Header/Header.jsx'
 import Footer from './components/Footer/Footer.jsx';
 import Food from './components/Food/Food.jsx';
+import Card from './components/Card/Card.jsx';
 
 function App() {
   return(
     <>
       <Header/>
-      <Food />
-      <Food />
-      <Food />
+      <Card />
+      <Card />
       <Footer/>
     </>
-    
   );
 }
 
