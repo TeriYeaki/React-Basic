288a3a088b2f8ce560acd1f01d5b12f1886a2e4b
TeriYeaki <118505370+TeriYeaki@users.noreply.github.com>
2024-06-12 16:53:45 +1000

    add the buttons to app

--- react-basic-app/src/App.jsx
+++ react-basic-app/src/App.jsx
@@ -2,13 +2,16 @@ import Header from './components/Header/Header.jsx'
 import Footer from './components/Footer/Footer.jsx';
 import Food from './components/Food/Food.jsx';
 import Card from './components/Card/Card.jsx';
+import Button from './components/Button/Button.jsx';
+import Button2 from './components/Button/Button2.jsx';
 
 function App() {
   return(
     <>
       <Header/>
       <Card />
-      <Card />
+      <Button />
+      <Button2 />
       <Footer/>
     </>
   );
