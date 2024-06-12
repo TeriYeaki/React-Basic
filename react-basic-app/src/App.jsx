2d851bde2a4a58190b2c5dda4d42b5aacac710d9
TeriYeaki <118505370+TeriYeaki@users.noreply.github.com>
2024-06-12 23:20:07 +1000

    created Student components using props and js default for default value

--- react-basic-app/src/App.jsx
+++ react-basic-app/src/App.jsx
@@ -4,12 +4,16 @@ import Food from './components/Food/Food.jsx';
 import Card from './components/Card/Card.jsx';
 import Button from './components/Button/Button.jsx';
 import Button2 from './components/Button/Button2.jsx';
+import StudentCard from './components/Card/StudentCard.jsx';
+import AvatarPic from './assets/images/SampleAvatar.png'
 
 function App() {
   return(
     <>
       <Header/>
       <Card />
+      <StudentCard imageSrc={AvatarPic} name="Tharith Yeak" description="A software engineering student at Monash."/>
+      <StudentCard />
       <Button />
       <Button2 />
       <Footer/>
